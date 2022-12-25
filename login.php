5b2e7f1326c307b796addd50df32c3a81a8292b7
Montek <montekkundan@gmail.com>
2022-12-24 18:09:19 -0800

    inital commit

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="styles/login.css">
+</head>
+<body>
+    <div>
+        <h1>Social Website</h1>
+
+    </div>
+    <div>
+        <h2>Log in</h2>
+        <div>
+            <input type="text" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <input type="button" value="Log in">
+        </div>
+    </div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+    <link rel="stylesheet" href="styles/signup.css">
+</head>
+<body>
+    <div>
+        <h1>Social Website</h1>
+
+    </div>
+    <div>
+        <h2>Sign Up</h2>
+        <div>
+            <input type="text" placeholder="First Name">
+            <input type="text" placeholder="Last Name">
+            <span>Gender:</span>
+            <select>
+                <option selected disabled hidden>Choose here</option>
+                <option>Male</option>
+                <option>Female</option>
+                <option>Other</option>
+                <option>Prefer not to say</option>
+            </select>
+            <input type="text" placeholder="Email">
+            <input type="password" placeholder="Password">
+            <input type="password" placeholder="Retype Password">
+            <input type="button" value="Sign up">
+        </div>
+    </div>
+</body>
+</html>
