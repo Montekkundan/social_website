3645414540ec916bae4c0d2ebc2d9f5b761e4e6d
Montek <montekkundan@gmail.com>
2022-12-24 20:40:17 -0800

    added user profile and timeline

--- timeline.php
+++ timeline.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
+    <title>Timeline</title>
     <link rel="stylesheet" href="styles/profile.css">
+    <link rel="stylesheet" href="styles/timeline.css">
 </head>
 <body>
     <div class="profile_nav_container">
@@ -16,30 +17,12 @@
         </div>
     </div>
     <div class="profile_cover_container">
-            <div class="profile__cover">
-                <img class="profile_cover_pic" src="assets/images/cover.jpeg" alt="cover pic">
-                <img class="profile_cover_profile_pic" src="assets/images/profile.jpeg" alt="profile pic">
-                <span class="profile__name">Name</span>
-                <div>
-                    <div class="menu__buttons">Timeline</div>
-                    <div class="menu__buttons">About</div>
-                    <div class="menu__buttons">Friends</div>
-                    <div class="menu__buttons">Photos</div>
-                    <div class="menu__buttons">Settings</div>
-                </div>
-            </div>
+            
             <div class="profile__content">
                 <div class="profile_content_left">
                     <div class="profile_content_friends">
-                       <span>Firends</span>
-                       <div class="friends__list">
-                            <img class="friends__img" src="assets/images/profile.jpeg" alt="">
-                            <span class="friend_name">First user</span>
-                       </div>
-                       <div class="friends__list">
-                            <img class="friends__img" src="assets/images/profile.jpeg" alt="">
-                            <span class="friend_name">First user</span>
-                       </div>
+                       <img class="timeline_profile_pic" src="assets/images/profile.jpeg" alt="timeline profile pic">
+                       <span class="timeline_profile_name">Name</span>
                     </div>
                 </div>
                 <div class="profile_content_right">
